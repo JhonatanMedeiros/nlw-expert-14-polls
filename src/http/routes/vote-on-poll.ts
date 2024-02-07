35cd4f8a0ef14451dd8e13f972f4b102ce990e88
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2024-02-07 20:34:28 -0300

    feat(poll-results): added pub/sub with ws in poll results

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import { randomUUID } from 'node:crypto';
 import { FastifyInstance } from "fastify"
 import { z } from "zod"
 import { prisma } from "../../lib/prisma"
+import { redis } from "../../lib/redis"
+import { voting } from '../../utils/voting-pub-sub';
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -29,13 +31,18 @@ export async function voteOnPoll(app: FastifyInstance) {
       })
 
       if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
-
         await prisma.vote.delete({
           where: {
             id: userPreviousVoteOnPoll.id
           }
         });
 
+        const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId);
+
+        voting.publish(pollId, {
+          pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+          votes: Number(votes),
+        });
       } else if (userPreviousVoteOnPoll) {
         return reply.status(400).send({
           message: 'User already voted on this poll'
@@ -62,8 +69,13 @@ export async function voteOnPoll(app: FastifyInstance) {
       }
     })
 
-    return reply.status(201).send({
-      sessionId,
-    });
+    const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes),
+    })
+
+    return reply.status(201).send();
   })
 }
